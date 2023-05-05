eb501f124ab6300053e73fbc07788847eb934739
akashKarmakar02 <akash.anan03@gmail.com>
2023-05-05 08:39:19 +0530

    Added add_venue view

--- events/views.py
+++ events/views.py
@@ -3,6 +3,8 @@ import calendar
 from calendar import HTMLCalendar
 from datetime import datetime
 from .models import Event
+from .forms import VenueForm
+from django.http import HttpResponseRedirect
 
 
 # Create your views here.
@@ -27,3 +29,18 @@ def all_events(request):
     event_list = Event.objects.all()
 
     return render(request, 'events/event_list.html', {"event_list": event_list})
+
+
+def add_venue(request):
+    submitted = False
+    if request.method == "POST":
+        form = VenueForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/add_venue?submitted=True')
+    else:
+        form = VenueForm
+        if 'submitted' in request.GET:
+            submitted = True
+
+    return render(request, 'events/add_venue.html', {'form': form, 'submitted': submitted})
