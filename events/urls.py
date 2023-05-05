6b45639e2a12f9244ab7baed61a17b887fecfc5f
akashKarmakar02 <akash.anan03@gmail.com>
2023-05-05 20:42:02 +0530

    added show_venue and list_venue urls

--- events/urls.py
+++ events/urls.py
@@ -4,7 +4,9 @@ from events import views
 
 urlpatterns = [
     path('<int:year>/<str:month>/', views.home),
+    path('venue/<venue_id>', views.show_venue, name="show-venue"),
     path('', views.home, name='home'),
     path('events', views.all_events, name="list-events"),
-    path('add_venue', views.add_venue, name="add-venue")
+    path('add_venue', views.add_venue, name="add-venue"),
+    path('list_venue', views.list_venue, name="list-venue"),
 ]
