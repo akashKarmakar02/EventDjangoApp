fa04d20de08a3145da3fcb70220e1abce86b54ba
akashKarmakar02 <akash.anan03@gmail.com>
2023-05-05 08:38:47 +0530

    Changed some venue fields to nullable

--- events/models.py
+++ events/models.py
@@ -1,13 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Venue(models.Model):
     name = models.CharField('Venue Name', max_length=120)
     address = models.CharField(max_length=300)
     zip_code = models.CharField('Zip Code', max_length=30)
-    phone = models.IntegerField('Phone no', max_length=14)
-    web = models.URLField('Web Site')
-    email_address = models.EmailField('Email Address', max_length=120)
+    phone = models.IntegerField('Phone no', blank=True)
+    web = models.URLField('Web Site', blank=True)
+    email_address = models.EmailField('Email Address', max_length=120, blank=True)
 
     def __str__(self):
         return self.name
@@ -26,7 +27,7 @@ class Event(models.Model):
     name = models.CharField('Event Name', max_length=120)
     event_date = models.DateTimeField('Event Date')
     venue = models.ForeignKey(Venue, blank=True, null=True, on_delete=models.CASCADE)
-    manager = models.CharField(max_length=60)
+    manager = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
     description = models.TextField(blank=True)
     attendees = models.ManyToManyField(MyClubUser, blank=True)
 
