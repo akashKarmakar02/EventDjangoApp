3fb46f62646d659bc832049e2ef6d136acf89ce0
akashKarmakar02 <akash.anan03@gmail.com>
2023-06-13 19:57:52 +0530

    Added login_required for protected urls

--- events/urls.py
+++ events/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 
 from events import views
 
@@ -13,8 +14,8 @@ urlpatterns = [
     path('venue_pdf', views.venue_pdf, name='venue-pdf'),
     path('', views.home, name='home'),
     path('events', views.all_events, name="list-events"),
-    path('add_venue', views.add_venue, name="add-venue"),
-    path('add_event', views.add_event, name="add-event"),
+    path('add_venue', login_required(views.add_venue), name="add-venue"),
+    path('add_event', login_required(views.add_event), name="add-event"),
     path('list_venue', views.list_venue, name="list-venue"),
     path('search_venues', views.search_venues, name="search-venues"),
 ]
