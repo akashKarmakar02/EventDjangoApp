b5bb0c31006dd7032f784f0935991bdd6addcbdc
akashKarmakar02 <akash.anan03@gmail.com>
2023-06-13 19:52:48 +0530

    added allauth urls

--- djangoProjects/urls.py
+++ djangoProjects/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from allauth.account.views import login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('events.urls')),
     path('members/', include('django.contrib.auth.urls')),
     path('members/', include('members.urls')),
+    path('accounts', include('allauth.urls'))
 ]
 
 admin.site.site_header = "My Club Administration Page"
