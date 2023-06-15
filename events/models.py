d16305d7bfc5ccd8fc2f49f3c66756bd7046f164
akashKarmakar02 <akash.anan03@gmail.com>
2023-06-15 10:47:39 +0530

    Changed venue model to have a owner

--- events/models.py
+++ events/models.py
@@ -2,18 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Venue(models.Model):
-    name = models.CharField('Venue Name', max_length=120)
-    address = models.CharField(max_length=300)
-    zip_code = models.CharField('Zip Code', max_length=30)
-    phone = models.IntegerField('Phone no', blank=True)
-    web = models.URLField('Web Site', blank=True)
-    email_address = models.EmailField('Email Address', max_length=120, blank=True)
-
-    def __str__(self):
-        return self.name
-
-
 class MyClubUser(models.Model):
     first_name = models.CharField('First Name', max_length=30)
     last_name = models.CharField('Last Name', max_length=30)
@@ -21,6 +9,19 @@ class MyClubUser(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+
+class Venue(models.Model):
+    name = models.CharField('Venue Name', max_length=120)
+    address = models.CharField(max_length=300)
+    zip_code = models.CharField('Zip Code', max_length=30)
+    phone = models.IntegerField('Phone no', blank=True)
+    web = models.URLField('Web Site', blank=True)
+    email_address = models.EmailField('Email Address', max_length=120, blank=True)
+    owner = models.ForeignKey(User, blank=False, on_delete=models.CASCADE, default=1)
+
+    def __str__(self):
+        return self.name
 
 
 class Event(models.Model):
